b9ab6df78f1e455315f76106ca4515fb1ce1a710
Azimjon <azimjonazs12@gmail.com>
2023-02-20 19:57:05 +0500

    fixed bug

--- lib/screens/card/screen_add_card.dart
+++ lib/screens/card/screen_add_card.dart
@@ -5,6 +5,7 @@ import 'package:shada_banking/widgets/card/CardNumberFormatter.dart';
 import 'package:shada_banking/widgets/card/DateFormatter.dart';
 import 'package:shada_banking/widgets/card/NameFormatter.dart';
 import 'package:shada_banking/widgets/card/card_item.dart';
+import 'package:shada_banking/widgets/card/custom_switch.dart';
 
 /// Creator : Azimjon Makhmudov
 /// Date : 2/20/2023 Time : 16:09
@@ -45,7 +46,40 @@ class _AddCardScreenState extends State<AddCardScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      backgroundColor: const Color.fromARGB(98, 232, 227, 227),
+      backgroundColor: const Color.fromARGB(228, 255, 254, 254),
+      appBar: AppBar(
+        title: const Text(
+          "Karta qo'shish",
+          maxLines: 1,
+          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print("yopish");
+            },
+            child: const Text(
+              "Yopish",
+              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          )
+        ],
+        leading: GestureDetector(
+          onTap: () {
+            print("back");
+          },
+          child: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.green,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,7 +121,7 @@ class _AddCardScreenState extends State<AddCardScreen> {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Text(
               "Karta raqami",
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
+              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 15),
             ),
           ),
           Container(
@@ -101,60 +135,99 @@ class _AddCardScreenState extends State<AddCardScreen> {
               keyboardType: TextInputType.number,
               controller: controllerCard,
               inputFormatters: [CardNumberFormatter()],
-              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-              decoration: const InputDecoration.collapsed(hintText: "Karta raqami", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 20)),
+              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+              decoration: const InputDecoration.collapsed(hintText: "Karta raqami", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
             ),
           ),
           const SizedBox(
             height: 15,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "Amal qilish muddati",
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-          ),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 20),
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(10))),
-            child: TextField(
-              maxLines: 1,
-              // maxLength: 16,
-              keyboardType: TextInputType.number,
-              controller: controllerDate,
-              inputFormatters: [DateFormatter()],
-              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-              decoration: const InputDecoration.collapsed(hintText: "OO/YY", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Amal qilish muddati",
+                        maxLines: 1,
+                        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        width: 80,
+                        decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(10))),
+                        child: TextField(
+                          maxLines: 1,
+                          keyboardType: TextInputType.number,
+                          controller: controllerDate,
+                          inputFormatters: [DateFormatter()],
+                          style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+                          decoration: const InputDecoration.collapsed(hintText: "OO/YY", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Karta nomi",
+                        maxLines: 1,
+                        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(10))),
+                        child: TextField(
+                          maxLines: 1,
+                          keyboardType: TextInputType.text,
+                          controller: controllerName,
+                          inputFormatters: [NameFormatter()],
+                          style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+                          decoration: const InputDecoration.collapsed(hintText: "Karta nomi", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
           const SizedBox(
             height: 15,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "Karta nomi",
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-          ),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 20),
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(10))),
-            child: TextField(
-              maxLines: 1,
-              keyboardType: TextInputType.text,
-              controller: controllerName,
-              inputFormatters: [NameFormatter()],
-              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-              decoration: const InputDecoration.collapsed(hintText: "Karta nomi", border: InputBorder.none, hintStyle: TextStyle(color: Colors.grey, fontSize: 20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Asosiy karta",
+                  maxLines: 1,
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                CustomSwitch(
+                  initialValue: true,
+                  onChanged: (value) {
+                    print('Switch value changed to $value');
+                  },
+                ),
+              ],
             ),
           ),
-          const Spacer(flex: 1,),
+          const Spacer(
+            flex: 1,
+          ),
+
           ButtonAuth("Karta qo'shish", controllerName.text.isNotEmpty && controllerDate.text.length == 5 && controllerCard.text.length == 19, () {
             //print("Karta qo'shish");
             Navigator.push(context,  MaterialPageRoute(builder: (context) => const IntroPage()));
